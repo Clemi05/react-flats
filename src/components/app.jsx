fcd722dffaebfe04be79823b70112c6afac6a3c7
Clement Azalbert <clementazalbert04@gmail.com>
2022-05-03 18:11:55 +0200

    started coding map + marker

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,6 @@
 // Components imports
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 import FlatList from './flat_list';
 import Marker from './marker';
 
@@ -19,6 +20,15 @@ class App extends Component {
     return (
       <div>
         <FlatList flats={this.state.flats} />
+        <div className="map-container">
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: "" }}
+            defaultCenter={defaultProps.center}
+            defaultZoom={defaultProps.zoom}
+          >
+          <Marker />
+        </GoogleMapReact>
+        </div>
       </div>
     );
   }
