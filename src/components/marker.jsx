de21d46fb3cb09a9702113f60dd87359246ce94d
Clement Azalbert <clementazalbert04@gmail.com>
2022-05-04 11:19:44 +0200

    updated Marker component

--- src/components/marker.jsx
+++ src/components/marker.jsx
@@ -1,11 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Marker extends Component {
-  render() {
-    return (
-      <h1>Marker</h1>
-    );
-  }
-}
+const Marker = () => <div className="marker">&nbsp;</div>;
 
 export default Marker;
